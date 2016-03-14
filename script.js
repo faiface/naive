79fc889164b00e2bfab1082c049d2d9b4dd7779e
faiface <faiface@ksp.sk>
2016-03-14 13:13:05 +0100

    implement expanding word tree

--- script.js
+++ script.js
@@ -11,19 +11,93 @@ var thoughtBoxTemplate =
 '   </div>\n' +
 '</div>\n'
 
+var wordTree = {}
+var paths = {};
+var index = 0;
+var activePath = []
+
+function getNode(tree, path) {
+    if (tree === undefined) return undefined;
+    if (path.length == 0) return tree;
+    return getNode(tree[path[0]], path.slice(1, path.length));
+}
+
+function deleteNode(node) {
+    if (node === undefined) return;
+    for (var child in node) {
+        delete node[child];
+    }
+    delete node;
+}
+
+function lightUpPath(path) {
+    $('.selected').removeClass('selected');
+
+    var currentPath = [];
+    for (var i = 0; i < path.length; i++) {
+        var $thoughtBox = getNode(wordTree, currentPath)['@'];
+        $thoughtBox.find('span').filter(function() {
+            return $(this).html() == path[i];
+        }).addClass('selected');
+        currentPath.push(path[i]);
+    }
+}
+
+function selectPath(path) {
+    var matchesUntil;
+    for (matchesUntil = 0; matchesUntil < path.length; matchesUntil++) {
+        if (matchesUntil >= activePath.length || path[matchesUntil] != activePath[matchesUntil]) {
+            break;
+        }
+    }
+
+    var promise;
+
+    for (var i = matchesUntil; i < activePath.length; i++) {
+        var $thoughtBox = getNode(wordTree, activePath.slice(0, i + 1))['@'];
+
+        $thoughtBox.slideUp('fast');
+        if (i == activePath.length - 1) {
+            promise = $thoughtBox.promise();
+        }
+    }
+
+    activePath = path;
+
+    var after = function() {
+        lightUpPath(path);
+
+        for (var i = matchesUntil; i < path.length; i++) {
+            var $thoughtBox = getNode(wordTree, path.slice(0, i + 1))['@'];
+            if ($thoughtBox === undefined) {
+                $thoughtBox = newThoughtBox(path.slice(0, i + 1));
+                $('.thoughts').append($thoughtBox);
+            }
+            $thoughtBox.slideDown('fast');
+        }
+    };
+
+    if (promise != undefined) promise.done(after);
+    else after();
+}
+
 function wordClick() {
-    //TODO
+    $thoughtBox = $(this).parent().parent().parent();
+    var path = paths[$thoughtBox.data('index')].slice();
+    path.push($(this).html());
+
+    selectPath(path);
 }
 
 function sentenceToWords(sentence) {
     var words = sentence.split(' ');
-    var filtered_words = [];
+    var filteredWords = [];
     words.forEach(function(word) {
         if (word.match(/[a-zA-Z]/i)) {
-            filtered_words.push(word);
+            filteredWords.push(word);
         }
-    })
-    return filtered_words
+    });
+    return filteredWords;
 }
 
 function wordsToSentence(words) {
@@ -44,6 +118,17 @@ function showInputBox($thoughtBox) {
 }
 
 function showWordsBox($thoughtBox) {
+    var path = paths[$thoughtBox.data('index')];
+    var node = getNode(wordTree, path);
+
+    selectPath(path);
+
+    for (var word in node) {
+        if (word != '@') {
+            deleteNode(node[word]);
+        }
+    }
+
     var sentence = $thoughtBox.children('.thought-input-box').children('.thought-input').val();
     var words = sentenceToWords(sentence);
 
@@ -51,12 +136,15 @@ function showWordsBox($thoughtBox) {
 
     var $thoughtWords = $thoughtBox.children('.thought-words-box').children('.thought-words');
     $thoughtWords.html('');
+
     words.forEach(function(word) {
         var $wordSpan = $('<span>');
         $wordSpan.addClass('thought-word');
         $wordSpan.html(word);
         $wordSpan.click(wordClick);
         $thoughtWords.append($wordSpan);
+
+        node[word] = {};
     });
 
     $thoughtBox.children('.thought-input-box').hide();
@@ -76,6 +164,10 @@ function editButtonClick() {
     showInputBox($thoughtBox);
 }
 
+function deleteButtonClick() {
+    // i do nothing on purpose
+}
+
 function thoughtInputKeydown(key) {
     if (key.which == 13) {  // enter key
         var $thoughtBox = $(this).parent().parent();
@@ -88,24 +180,30 @@ function thoughtInputOnblur() {
     showWordsBox($thoughtBox);
 }
 
-function newThoughtBox(parents) {
+function newThoughtBox(path) {
     var $thoughtBox = $(thoughtBoxTemplate);
-    $thoughtBox.attr('parents', parents);
+    $thoughtBox.data('index', index);
+    $thoughtBox.hide();
 
     var $thoughtInput = $thoughtBox.children('.thought-input-box').children('.thought-input');
     $thoughtInput.keydown(thoughtInputKeydown);
     $thoughtInput.blur(thoughtInputOnblur);
 
-    if (parents.length > 0) {
-        $thoughtInput.attr('placeholder', 'What is ' + parents[parents.length - 1] + '?');
+    if (path.length > 0) {
+        $thoughtInput.attr('placeholder', 'What is ' + path[path.length - 1] + '?');
     } else {
         $thoughtInput.attr('placeholder', 'Clear your mind...');
     }
 
     var $thoughtWordsBox = $thoughtBox.children('.thought-words-box');
     $thoughtWordsBox.children('.edit-button').click(editButtonClick);
+    $thoughtWordsBox.children('.delete-button').click(deleteButtonClick);
 
     $thoughtWordsBox.hide();
+
+    paths[index++] = path;
+    var treeNode = getNode(wordTree, path);
+    treeNode['@'] = $thoughtBox;
 
     return $thoughtBox;
 }
@@ -124,7 +222,10 @@ function introToggle() {
 
 function main() {
     $('#intro-toggle').click(introToggle);
-    $('.thoughts').append(newThoughtBox([]));
+
+    var $firstThoughtBox = newThoughtBox([]);
+    $firstThoughtBox.show();
+    $('.thoughts').append($firstThoughtBox);
 }
 
 $(document).ready(main);
